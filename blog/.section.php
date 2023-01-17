3a30c07692941ab7577c6183c34413e1ce7169e5
EvgenyRomanov <e.romanov93@yandex.ru>
2023-01-17 18:42:19 +0500

    first commit

---
+++ blog/.section.php
@@ -0,0 +1,9 @@
+<?
+$sSectionName = "Блог";
+$arDirProperties = Array(
+   "description" => "Описание блога",
+   "keywords" => "Ключевики...",
+   "title" => "Блог - список статей",
+   "keywords_inner" => "Блог"
+);
+?>
---
+++ local/templates/voguish_single/description.php
@@ -0,0 +1,6 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<?
+$arTemplate = Array(
+	"NAME" => 'Шаблон главной страницы без сайдбара', 
+	"DESCRIPTION" => 'Описание шаблона', 
+);?>
